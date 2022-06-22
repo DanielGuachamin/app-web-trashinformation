783587870596742549dd610d9ec67c054b0cc15e
DanielGuachamin <guillermo.guachamin@epn.edu.ec>
2022-06-22 09:54:22 -0500

    CRUD noticias v.2

--- src/app/components/adminpages/dashboard-admin/news-admin/news-admin.component.ts
+++ src/app/components/adminpages/dashboard-admin/news-admin/news-admin.component.ts
@@ -22,16 +22,19 @@ export class NewsAdminComponent implements OnInit {
   noticias: Noticia[] = [];
   urlNoticia: string = '';
   fillNoticia: Noticia[] = [];
-  
-  plantillaImage:any = {
-    MedioAmbiente: 'https://firebasestorage.googleapis.com/v0/b/trash-information-appweb.appspot.com/o/plantillaImages%2Fmedioambiente.jpg?alt=media&token=cf74d9ba-7683-4d60-b742-5141625c4057',
-    Orgánicos: 'https://firebasestorage.googleapis.com/v0/b/trash-information-appweb.appspot.com/o/plantillaImages%2Forganicos.jpg?alt=media&token=925c173b-8a51-4037-965d-b25062629c13',
-    Reciclaje: 'https://firebasestorage.googleapis.com/v0/b/trash-information-appweb.appspot.com/o/plantillaImages%2Freciclaje.jpg?alt=media&token=7bd9d00e-0fae-4e30-aaff-71dbeca12b4f',
-    Covid19: 'https://firebasestorage.googleapis.com/v0/b/trash-information-appweb.appspot.com/o/plantillaImages%2Fcovid19.jpg?alt=media&token=9330e991-2f82-4ba5-a2fe-d0db316998dd'
+
+  plantillaImage: any = {
+    MedioAmbiente:
+      'https://firebasestorage.googleapis.com/v0/b/trash-information-appweb.appspot.com/o/plantillaImages%2Fmedioambiente.jpg?alt=media&token=cf74d9ba-7683-4d60-b742-5141625c4057',
+    Orgánicos:
+      'https://firebasestorage.googleapis.com/v0/b/trash-information-appweb.appspot.com/o/plantillaImages%2Forganicos.jpg?alt=media&token=925c173b-8a51-4037-965d-b25062629c13',
+    Reciclaje:
+      'https://firebasestorage.googleapis.com/v0/b/trash-information-appweb.appspot.com/o/plantillaImages%2Freciclaje.jpg?alt=media&token=7bd9d00e-0fae-4e30-aaff-71dbeca12b4f',
+    Covid19:
+      'https://firebasestorage.googleapis.com/v0/b/trash-information-appweb.appspot.com/o/plantillaImages%2Fcovid19.jpg?alt=media&token=9330e991-2f82-4ba5-a2fe-d0db316998dd',
   };
 
   constructor(private dataControl: DataApiService, private storage: Storage) {
-
     this.formNoticia = new FormGroup({
       title: new FormControl(),
       category: new FormControl(),
@@ -46,55 +49,52 @@ export class NewsAdminComponent implements OnInit {
   ngOnInit(): void {
     this.dataControl.getNoticias().subscribe((noticias) => {
       //console.log(noticias);
-      this.noticias = noticias
-      this.enumNoticias = noticias.length
+      this.noticias = noticias;
+      this.enumNoticias = noticias.length;
     });
   }
 
   async onSubmitAddNoticia() {
     //console.log('formulario noticia: ', this.formNoticia.value)
-    const idAdd = this.comprobarIdNoticia()
-    const nameNoticiaImage =this.selectedFile
-    
-    
-    if(nameNoticiaImage == null){
-      const baseImages = this.plantillaImage
-      const category = this.formNoticia.get('category').value
-      console.log(category)
-      for(let nameImage in baseImages){
-        if(nameImage == category){
-          
-          const responseUrlImage = baseImages[nameImage]
-          
+    const idAdd = this.comprobarIdNoticia();
+    const nameNoticiaImage = this.selectedFile;
+
+    if (nameNoticiaImage == null) {
+      const baseImages = this.plantillaImage;
+      const category = this.formNoticia.get('category').value;
+      console.log(category);
+      for (let nameImage in baseImages) {
+        if (nameImage == category) {
+          const responseUrlImage = baseImages[nameImage];
+
           this.formNoticia.controls['noticiaPic'].setValue(responseUrlImage);
         }
       }
-    }else{
-      const urlNoticia = this.urlNoticia
+    } else {
+      const urlNoticia = this.urlNoticia;
       this.formNoticia.controls['noticiaPic'].setValue(urlNoticia);
-
     }
-    this.formNoticia.controls['id'].setValue(idAdd)
+
+    this.formNoticia.controls['id'].setValue(idAdd);
     await this.dataControl.addNoticia(this.formNoticia.value, idAdd);
     this.formNoticia.reset();
   }
 
-  comprobarIdNoticia(){
-    const listNoticia = this.noticias
-    const idNoticiaBD = listNoticia.map(item => item.id);
-    const idMod = this.formNoticia.get('id').value
+  comprobarIdNoticia() {
+    const listNoticia = this.noticias;
+    const idNoticiaBD = listNoticia.map((item) => item.id);
+    const idMod = this.formNoticia.get('id').value;
     let idAdd;
-    for(let item of idNoticiaBD) {
-      if(item == idMod){
-        idAdd = idMod
-        return idAdd
-      }
-      else{
-        idAdd = `${this.enumNoticias + 1}n`;
-        return idAdd
+    for (let item of idNoticiaBD) {
+      if (item == idMod) {
+        
+        idAdd = idMod;
+
+        return idAdd;
       }
     }
-   
+    idAdd = `${this.enumNoticias + 1}n`;
+    return idAdd;
   }
 
   async deleteNoticiaById(id: any) {
@@ -109,28 +109,27 @@ export class NewsAdminComponent implements OnInit {
 
     uploadBytes(imgRef, file)
       .then((response) => {
-        console.log(response)
-        this.getNoticiaImageUrl(`noticiasImages/${file.name}`)
-        
+        console.log(response);
+        this.getNoticiaImageUrl(`noticiasImages/${file.name}`);
       })
       .catch((error) => console.log(error));
   }
 
-  getNoticiaImageUrl(path:string){
-    getDownloadURL(ref(this.storage, path))
-    .then((url) => {
+  getNoticiaImageUrl(path: string) {
+    getDownloadURL(ref(this.storage, path)).then((url) => {
       //console.log('Url de la imagen: ', url)
-      this.urlNoticia = url
-    })
+      this.urlNoticia = url;
+    });
   }
 
-  fillFormNoticia(id: any){
-    this.dataControl.modifiedNoticia(id)
-      .then((response:any) => {
-        const FormNoticia = response
-        this.formNoticia.setValue(response)
-        
-      })
+  fillFormNoticia(id: any) {
+    this.dataControl.modifiedNoticia(id).then((response: any) => {
+      const FormNoticia = response;
+      this.formNoticia.setValue(response);
+    });
   }
-  
+
+  clearForm(){
+    this.formNoticia.reset();
+  }
 }
